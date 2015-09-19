453474425263b61f3f2ea45b519b8051312cb30d
JanRuettinger <jan-hendrik.ruettinger@online.de>
2015-09-19 04:44:48 -0400

    Added create charity in main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,15 +2,25 @@ class MainController < ApplicationController
 
   def index
     @item=Item.new
+    @charityorg = CharityOrg.new
   end
 
-  def create
+  def create_item
   @item=Item.new(item_params)
     if @item.save
       redirect_to action: "thank_you_show"
     else
       render "donate_money_show"
     end
+  end
+
+  def create_charityorg
+    @charityorg = CharityOrg.new(charity_params)
+      if @charityorg.save
+        edirect_to action: "thank_you_show"
+      else
+        render "donate_money_show"
+      end
   end
 
   def thank_you_show
@@ -22,6 +32,10 @@ class MainController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :email, :address, :url, :zip, :description)
+  end
+
+  def charity_params
+    params.require(:charityorg).permit(:email, :url, :password, :zip, :name, :address)
   end
 
 
