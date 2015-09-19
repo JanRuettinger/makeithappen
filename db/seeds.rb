784a3cc8fd0ae2b8d574a88b69bed0cf069c02c1
JanRuettinger <jan-hendrik.ruettinger@online.de>
2015-09-19 17:54:33 -0400

    Almost working Monez calculation problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first
-Item.create(name: "T-Shirt", address: "Hausmannstrasse 12a", zip: "10027", requested: "0",
+Item.create(name: "T-Shirt", address: "Marienplatz 8", zip: "80331", requested: "1",
             url: "google.com", email: "test@test.com")
 
-Item.create(name: "Pants", address: "Hausmannstrasse 12a", zip: "10027", requested: "1",
+Item.create(name: "Pants", address: "Nordallee 25", zip: "85356", requested: "1",
             url: "google.com", email: "test2@test.com")
 
-Item.create(name: "Underwear", address: "Hausmannstrasse 12a", zip: "10028", requested: "0",
+Item.create(name: "Underwear", address: "Geschwister-Scholl-Platz 1", zip: "80539", requested: "1",
             url: "google.com", email: "test3@test.com")
 
-Item.create(name: "Shorts", address: "Hausmannstrasse 12a", zip: "10038", requested: "1",
+Item.create(name: "Shorts", address: "Am Olympiapark 1", zip: "80809", requested: "0",
             url: "google.com", email: "test4@test.com")
 
-Item.create(name: "Shoes", address: "Hausmannstrasse 12a", zip: "10029", requested: "0",
+Item.create(name: "Shoes", address: "Arcisstraße 21", zip: "80333", requested: "0",
             url: "google.com", email: "test5@test.com")
 
-Item.create(name: "Socks", address: "Hausmannstrasse 12a", zip: "10029", requested: "0",
+Item.create(name: "Socks", address: "HMessegelände", zip: "81823", requested: "0",
             url: "google.com", email: "test6@test.com")
 
 
 
-CharityOrg.create(name: "Rotes Kreuz", address: "Im Garten 12a", zip: "10027", password: "test",
+CharityOrg.create(name: "Rotes Kreuz", address: "Garmischer Straße 19", zip: "81373", password: "test",
             url: "google.com", email: "test1@test.com")
 
-CharityOrg.create(name: "Greenpeace", address: "Fussballplatz 37", zip: "10028", password: "test",
+CharityOrg.create(name: "Greenpeace", address: "Frohschammer Straße 14", zip: "80807", password: "test",
             url: "google.com", email: "test2@test.com")
 
-CharityOrg.create(name: "Rettet die Bienen", address: "nStadio 10", zip: "10029", password: "test",
+CharityOrg.create(name: "Rettet die Bienen", address: "Nymphenburger Str. 64", zip: "80335", password: "test",
             url: "google.com", email: "test3@test.com")
