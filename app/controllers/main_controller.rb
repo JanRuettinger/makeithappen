d9b3fdacdce23bdaed9097c3ecf7e92561def397
Michael Stella <michael@michael-stella.info>
2015-09-19 17:01:44 -0400

    logic for money donate

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,20 @@ class MainController < ApplicationController
   def index
     @item=Item.new
     @charityorg = CharityOrg.new
+    
+    @itemsRequested=Item.all.where("requested > 0")
+    
+  end
+
+  def charityRequested(i)
+  	requested = i.requested
+  	return CharityOrg.find(requested)
+  end
+  
+  def estimateCost(i)
+  	start_address = (i.address).concat(i.zip)
+  	end_address = charityRequested(i).address.concat(charityRequested(i).zip)
+  	return EstimateCalc.new.getUberXPrice(start_address, end_address)
   end
 
   def thank_you_show
