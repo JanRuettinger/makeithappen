65fa3f0884890abea18ed8d767eb1ede2d6ed3d3
Michael Stella <michael@michael-stella.info>
2015-09-19 22:08:37 -0400

    Working

--- app/controllers/possibleitems_controller.rb
+++ app/controllers/possibleitems_controller.rb
@@ -2,11 +2,14 @@ class PossibleitemsController < ApplicationController
 
   def calculateorder
 
+	print session[:charityorg_id]
+	
     @items = []
     @cost = []
     itemsall = Item.all.where("requested = 0")
     itemsall.each do |item|
-      c = item.estimate_cost_for_charity(current_charity())
+      c = item.estimate_cost_for_charity(CharityOrg.find_by(id: session[:charityorg_id]))
+      print c
       if c != "-1"
           @items.push(item)
           @cost.push(c)
