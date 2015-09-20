76e5d8ae4c2b9620f9ac7249d404240b3c953ec0
Michael Stella <michael@michael-stella.info>
2015-09-20 01:11:41 -0400

    Design Changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,7 @@ class Item < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :name,  presence: true
+                    format: { with: VALID_EMAIL_REGEX }
   validates :requested, presence: true
   validates :address,  presence: true
   validates :zip,  presence: true,length: { maximum: 7 }
